dae42606378e03e6f65382e0818efc86fdfafadc
sierrataylor <68617779+sierrataylor@users.noreply.github.com>
2022-09-15 13:27:42 -0400

    Change .gitignore so Webpack files are not committed (#22)
    * Add skeleton code for controllers based on db schema
    
    * Add controllers to api and app
    
    * Delete Controllers directory
    
    * Add Entities folder to app
    
    * Add interfaces skeleton classes to API
    
    * Started writing HTTP GET action in API/Controllers/QuestionController.cs
    
    * Add webpack files to .gitignore
    
    * Change Entities folder to Models

---
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionApp/Models/Equipment.cs
@@ -0,0 +1,11 @@
+﻿namespace SafetyEquipmentInspectionAPP.Models
+{
+    public class Equipment
+    {
+        public virtual int EquipmentID { get; set; }
+        public virtual string EquipmentType { get; set; }
+        public virtual string Location { get; set; }
+        public virtual string Building { get; set; }
+        public virtual string Floor { get; set; }
+    }
+}
