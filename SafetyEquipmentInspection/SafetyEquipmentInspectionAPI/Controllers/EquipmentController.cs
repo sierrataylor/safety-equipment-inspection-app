cd0f163eddd881b9c4be8dad9d755c5c109267cd
sierrataylor <staylor23@jcu.edu>
2022-09-20 19:31:40 -0400

    Add get method to Equipment controller

--- SafetyEquipmentInspection/SafetyEquipmentInspectionAPI/Controllers/EquipmentController.cs
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionAPI/Controllers/EquipmentController.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using SafetyEquipmentInspectionAPI.Constants;
 using SafetyEquipmentInspectionAPI.DTOs;
 using SafetyEquipmentInspectionAPI.Interfaces;
+using System.Web;
 
 namespace SafetyEquipmentInspectionAPI.Controllers
 {
@@ -14,6 +15,46 @@ namespace SafetyEquipmentInspectionAPI.Controllers
         {
             Environment.SetEnvironmentVariable(FirestoreConstants.GoogleApplicationCredentials, FirestoreConstants.GoogleApplicationCredentialsPath);
             _db = FirestoreDb.Create(FirestoreConstants.ProjectId);
+        }
+
+        [HttpGet("/equipment/item/{id}")]
+        public async Task<string> GetItem(int id)
+        {
+            //get Equipment collection from NoSQL db
+            var equipmentcollection = _db.Collection("Equipment"); 
+
+            //query collection for document with an EquipmentId equal to id and get async snapshot of query result
+            var query = await equipmentcollection.WhereEqualTo("EquipmentId", id.ToString()).GetSnapshotAsync(); 
+
+            try
+            {
+                if (query.Documents.Count > 0)
+                {
+
+                    //get async snapshot of this document; null if query.document.Count = 0 (meaning the equipmentId was not found)
+                    var itemDoc = await equipmentcollection.Document(query.Documents[0].Id).GetSnapshotAsync();
+
+                    /*if the document exists convert it to a dictionary
+                     * serialize dictionary to JSON
+                     * deserialize that JSON to a DTO
+                     * and return the json string of the dictionary*/
+                    Dictionary<string, object> result = itemDoc.ToDictionary();
+                    var resultJson = JsonConvert.SerializeObject(result);
+                    EquipmentDto equipmentDto = JsonConvert.DeserializeObject<EquipmentDto>(resultJson);
+                    return resultJson;
+
+                }
+                else
+                {
+                    //if document is not found
+                    return JsonConvert.SerializeObject(new { id = id, error = $"Item with ID {id} not found"});
+                }
+            }
+            catch (Exception ex)
+            {
+                //if document 
+                return JsonConvert.SerializeObject(new {error = ex.Message});
+            }
         }
 
         [HttpPost("AddEquipmentPiece")]
