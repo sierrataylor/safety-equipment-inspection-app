2a16b87e105d349a24afa91ca7ee13415c4da7c4
carriecuva <ccuva23@jcu.edu>
2022-09-25 17:33:09 -0400

    got rid of the code that allowed me to check which function i was on as that was unnecessary

--- SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/assets/qrCodeScanner.js
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/assets/qrCodeScanner.js
@@ -20,11 +20,9 @@ qrcode.callback = res => {
 
 function initializescanner() {
   const canvas = document.getElementById("qr-canvas");
-  document.getElementById("hello").innerHTML = "Hello friends";
   navigator.mediaDevices
     .getUserMedia({ video: { facingMode: "environment" } })
     .then(function (stream) {
-      document.getElementById("hello").innerHTML = "got to stream function";
       document.getElementById("scannerimg").style.display = "none";
       // canvas.style.display = block;
       video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
@@ -39,19 +37,14 @@ function initializescanner() {
 
 function tick() {
   var canvas = document.getElementById("qr-canvas");
-  document.getElementById("hello").innerHTML = "got to tick function";
   canvas.height = video.videoHeight;
-  document.getElementById("hello").innerHTML = "got to point of height of canvas defined";
   canvas.width = video.videoWidth;
-  document.getElementById("hello").innerHTML = "got to point of width of canvas defined";
   canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
 
    requestAnimationFrame(tick);
-  document.getElementById("hello").innerHTML = "got to request animtion frame";
 }
 
 function scan() {
-  document.getElementById("hello").innerHTML = "got to scan function";
   try {
     qrcode.decode();
   } catch (e) {
