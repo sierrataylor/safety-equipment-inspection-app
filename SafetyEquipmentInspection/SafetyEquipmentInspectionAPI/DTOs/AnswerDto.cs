07a9af539e16a9bd6af6c75b5d02e705cba4df60
sierrataylor <staylor23@jcu.edu>
2022-09-29 15:13:25 -0400

    Add Firestore attributes to DTOs

--- SafetyEquipmentInspection/SafetyEquipmentInspectionAPI/DTOs/AnswerDto.cs
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionAPI/DTOs/AnswerDto.cs
@@ -1,10 +1,17 @@
-﻿namespace SafetyEquipmentInspectionAPI.DTOs
+﻿using Google.Cloud.Firestore;
+
+namespace SafetyEquipmentInspectionAPI.DTOs
 {
+    [FirestoreData]
     public class AnswerDto
     {
+        [FirestoreProperty]
         public Guid AnswerId { get; set; }
+        [FirestoreProperty]
         public string EquipmentId { get; set; }
+        [FirestoreProperty]
         public int QuestionNumber { get; set; }
+        [FirestoreProperty]
         public string Response { get; set; }
     }
 }
--- SafetyEquipmentInspection/SafetyEquipmentInspectionAPI/DTOs/EmployeeDto.cs
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionAPI/DTOs/EmployeeDto.cs
@@ -1,11 +1,19 @@
-﻿namespace SafetyEquipmentInspectionAPI.DTOs
+﻿using Google.Cloud.Firestore;
+
+namespace SafetyEquipmentInspectionAPI.DTOs
 {
+    [FirestoreData]
     public class EmployeeDto
     {
+        [FirestoreProperty]
         public string EmployeeId { get; set; }
+        [FirestoreProperty]
         public string FirstName { get; set; }
+        [FirestoreProperty]
         public string LastName { get; set; }
+        [FirestoreProperty]
         public string Email { get; set; }
+        [FirestoreProperty]
         public string Role { get; set; }
     }
 }
