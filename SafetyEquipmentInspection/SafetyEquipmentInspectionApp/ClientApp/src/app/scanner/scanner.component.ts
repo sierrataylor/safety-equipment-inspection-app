73646c778bbadaa1a3ec18ea80ce2b42f4c5a60e
carriecuva <ccuva23@jcu.edu>
2022-10-10 02:20:44 -0400

    This was my most recent attempt to get url from qr code to appear next to data: . Tomorrow I will continue with this. Research online showed that angular may not be able to read script tags correctly and it may need to be added like this

--- SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/scanner/scanner.component.ts
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/scanner/scanner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 declare function initalizescanner(): any;
 declare function submit(): any;
 @Component({
@@ -8,10 +10,15 @@ declare function submit(): any;
 })
 export class ScannerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer2: Renderer2,
+   @Inject(DOCUMENT) private _document: Document) { }
 
-  ngOnInit(): void {
-    
+  ngOnInit() {
+ 
+    const s = this.renderer2.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://rawgit.com/sitepoint-editors/jsqrcode/master/src/qr_packed.js';
+    this.renderer2.appendChild(this._document.body, s);
   }
 
 }
