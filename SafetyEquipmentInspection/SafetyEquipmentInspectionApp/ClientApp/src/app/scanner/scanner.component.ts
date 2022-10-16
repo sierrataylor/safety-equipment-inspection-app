b30304b287c2e5d16f63d7073b89237564967dcc
Adam Brennan <adamjohnbrennancs@gmail.com>
2022-10-16 16:59:12 -0400

    Fixed QR Scanner
    - Added minified instascan.js script to angular.json
    - Added code utilizing instascan package to qrCodeReader.js
    - Added appropriate html in scanner.component.html
    - Removed old CSS from scanner.component.css

--- SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/scanner/scanner.component.ts
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/scanner/scanner.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-declare function initalizescanner(): any;
-declare function submit(): any;
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
@@ -11,7 +9,6 @@ export class ScannerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
   }
 
 }
