1942f651dccca999e4bfa3d8e9241ad739262ee3
rboland01 <81589584+rboland01@users.noreply.github.com>
2022-10-17 00:36:29 -0400

    Updated Log In page with beginning validation

--- SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/log-in/log-in.component.ts
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/log-in/log-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+import { EmployeeDto } from '../SharedDTO/employee.dto';
+
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  Employee: EmployeeDto | undefined;
+  employeeId: string = "";
+  employeePassword: string = "";
+
+
+  constructor(public service: SharedService) { }
 
   ngOnInit(): void {
+  }
+
+  LogInUser() {
+    this.GetEmployee(this.employeeId);
+    
+  }
+
+  GetEmployee(employeeId: any) {
+    console.log(employeeId);
+    return this.service.GetEmployee(employeeId).subscribe(data => {
+      this.Employee = data;
+      console.log(this.Employee);
+      console.log(this.Employee.employeeId);
+      console.log(this.Employee.email);
+    })
   }
 
 }
