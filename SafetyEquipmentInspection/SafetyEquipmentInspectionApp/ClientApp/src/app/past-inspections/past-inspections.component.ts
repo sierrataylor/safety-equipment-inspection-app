2890790e6420235f25ecc5fc57534b7b800aa25a
carriecuva <ccuva23@jcu.edu>
2022-12-08 12:07:01 -0500

    Updates: 1) past inspections page is formatted a little different now has an info button to display all the questions. Going to all show answers as well when I code that) 2) to-do : I thought I found code that would work but could ntot figure out rest api that was able to filter to only dates that are 30 days or more behind the current date.

--- SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/past-inspections/past-inspections.component.ts
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/past-inspections/past-inspections.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SharedService } from 'src/app/shared.service';
 import { InspectionDto } from '../SharedDTO/inspection.dto';
+import { QuestionDto } from '../SharedDTO/question.dto';
+
 
 @Component({
   selector: 'app-past-inspections',
@@ -9,18 +11,42 @@ import { InspectionDto } from '../SharedDTO/inspection.dto';
   styleUrls: ['./past-inspections.component.css']
 })
 export class PastInspectionsComponent implements OnInit {
+  
   readonly APIUrl = "https://localhost:7023/";
   inspections: InspectionDto[] | any;
+  questions: QuestionDto[] | any;
 
   constructor(public service: SharedService) { }
 
   ngOnInit(): void {
     this.GetInspectionsList();
+    this.GetQuestionsList();
   }
   GetInspectionsList() {
     return this.service.GetInspectionsList().subscribe(data => {
       this.inspections = data;
     });
-  }
+}
+  GetQuestionsList(equipmentType: any = "") {
+    return this.service.GetQuestionsList(equipmentType).subscribe(data => {
+      this.questions = data;
+    });
+    }
 
+  public openInfo()
+  {
+    const scan = document.getElementById('scaninfoId');
+    if (scan != null) {
+      scan.style.display = 'block';
+    }
+    
+  }
+  public closeInfo() {
+    const scan = document.getElementById('scaninfoId');
+    if (scan != null) {
+      scan.style.display = 'block';
+      
+    }
+
+  }
 }
