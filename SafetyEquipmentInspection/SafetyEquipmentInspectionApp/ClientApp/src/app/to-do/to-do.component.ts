9bf39ce0e260a86f5ccf85bda705411e950bff0f
carriecuva <ccuva23@jcu.edu>
2022-12-12 23:50:32 -0500

    changes to past inspections page and to-do pages

--- SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/to-do/to-do.component.ts
+++ SafetyEquipmentInspection/SafetyEquipmentInspectionApp/ClientApp/src/app/to-do/to-do.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SharedService } from 'src/app/shared.service';
-
+import { InspectionDto } from '../SharedDTO/inspection.dto';
 
 @Component({
   selector: 'app-scanner',
@@ -10,10 +10,16 @@ import { SharedService } from 'src/app/shared.service';
 })
 export class ToDoComponent implements OnInit {
 
+  readonly APIUrl = "https://localhost:7023/";
+  inspections: InspectionDto[] | any;
   constructor(public service: SharedService) {}
 
   ngOnInit(): void {
-   
+    this.GetToDoList();
   }
- 
+  GetToDoList(equipmentId: any = "") {
+    return this.service.GetToDoList(equipmentId).subscribe(data => {
+      this.inspections = data;
+    });
+  }
 }
